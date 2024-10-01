116928e40e25b3520625a76816971f2d60a0090b
Thomas Ströder <thomas.stroeder@email.de>
2024-10-01 13:00:18 +0100

    more show types

--- src/main/java/lectureinitializer/CalendarEntryShowType.java
+++ src/main/java/lectureinitializer/CalendarEntryShowType.java
@@ -6,8 +6,12 @@ public enum CalendarEntryShowType {
 
     public static CalendarEntryShowType parse(final String showType) {
         switch (Integer.parseInt(showType)) {//TODO
+        case 1:
+            return TENTATIVE;
         case 2:
             return BLOCKED;
+        case 3:
+            return FREE;
         case 4:
             return ELSEWHERE;
         }
