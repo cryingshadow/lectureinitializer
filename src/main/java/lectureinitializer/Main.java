30aaabf30fa4b7f1778bd88aca006978c0f4c7c5
Thomas Ströder <thomas.stroeder@email.de>
2025-05-28 08:39:21 +0100

    don't add letters to answers anymore

--- src/main/java/lectureinitializer/Main.java
+++ src/main/java/lectureinitializer/Main.java
@@ -477,14 +477,14 @@ public class Main {
                 int skip = random.nextInt(4);
                 for (int i = 0; i < 3; i++) {
                     writer.write("\\item ");
-                    writer.write('A' + i + (skip < 0 ? 1 : 0));
-                    writer.write(") ");
+//                    writer.write('A' + i + (skip < 0 ? 1 : 0));
+//                    writer.write(") ");
                     if (skip == 0) {
                         correctAnswers.add((char)('a' + i));
                         writer.write(question.correctAnswer());
                         writer.write("\n\\item ");
-                        writer.write('A' + i + 1);
-                        writer.write(") ");
+//                        writer.write('A' + i + 1);
+//                        writer.write(") ");
                     }
                     writer.write(question.wrongAnswers().get(i));
                     skip--;
@@ -492,7 +492,8 @@ public class Main {
                 }
                 if (skip == 0) {
                     correctAnswers.add('d');
-                    writer.write("\\item D) ");
+                    writer.write("\\item ");
+//                    writer.write("D) ");
                     writer.write(question.correctAnswer());
                     writer.write("\n");
                 }
